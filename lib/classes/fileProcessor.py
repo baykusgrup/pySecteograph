2365000c06efffc4780fc03bb82f1ba0d9248a0f
Serbay ACAR <serbayacar@gmail.com>
2019-07-21 21:27:56 +0300

    File :: write/append/read

---
+++ lib/classes/fileProcessor.py
@@ -0,0 +1,50 @@
+from pathlib import Path
+
+
+class FileProcessor:
+    path = './test'
+    permissions = 'w+'
+    fileBuffer = None
+
+    def __init__(self, path='./test', permissions='w+'):
+        self.path = path
+        self.permissions = permissions
+        self.getFile()
+
+    def getFile(self):
+        todo_file = Path(self.path)
+        if todo_file.is_file():
+            self.fileBuffer = open(todo_file, 'a+')
+        else:
+            self.fileBuffer = open(todo_file, self.permissions)
+
+    def closeFile(self):
+        self.fileBuffer = self.fileBuffer.close()
+
+    def write(self, data, force=False):
+        try:
+            with self.fileBuffer as f:
+                # To-do ID -- User -- Description -- StartDate -- EndDate -- Status
+                f.write(
+                    "{0} {1} {2} {3} {4} {5} \n".format(
+                        data['ID'],
+                        data['user'],
+                        data['desc'],
+                        data['start'],
+                        data['end'],
+                        data['status']
+                    )
+                )
+                # f.write("text\n")
+        except IOError as identifier:
+            pass
+
+    def read(self, force=False):
+        try:
+            with self.fileBuffer as f:
+                f.seek(0, 0)
+                lines = f.readlines()
+                for line in lines:
+                    print(line)
+        except IOError as identifier:
+            pass
--- lib/classes/secteograph.py
+++ lib/classes/secteograph.py
@@ -1,3 +1,6 @@
+from .fileProcessor import FileProcessor
+
+
 class Secteograph:
     x = 'deneme'
 
@@ -9,8 +12,12 @@ class Secteograph:
 
     @staticmethod
     def addTodo(start, end, desc, user):
-        print('Start: %s End: %s Desc: %s User: %s' %
-              (start, end, desc, user))
+        data = {"ID": "1", "user": user, "desc": desc,
+                "start": start, "end": end, "status": "OPEN"}
+        fp = FileProcessor()
+        fp.write(data)
+        # print('Start: %s End: %s Desc: %s User: %s' %
+        #       (start, end, desc, user))
 
     @staticmethod
     def deleteTodo(user, start, end, desc):
@@ -19,6 +26,8 @@ class Secteograph:
 
     @staticmethod
     def getTodo(user, start, end, desc):
+        fp = FileProcessor()
+        fp.read()
         print('Start: %s End: %s Desc: %s User: %s' %
               (start, end, desc, user))
 
